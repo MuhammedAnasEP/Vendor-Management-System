52d17e1bd07aadcc8d60587b74b521f62be46b81
MuhammedAnasEP <anasep1110@gmail.com>
2023-12-29 10:28:17 +0530

    added some functionality in models

--- vendors/models.py
+++ vendors/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -15,6 +16,27 @@ class Vendor(models.Model):
     def __str__(self):
         return self.name
     
+    def calculate_on_time_delivery_rate(self):
+        completed_pos = self.purchaseorder_set.filter(status='completed')
+        on_time_deliveries = completed_pos.filter(delivery_date__lte=timezone.now()).count()
+        total_completed_pos = completed_pos.count()
+        return (on_time_deliveries / total_completed_pos) * 100 if total_completed_pos > 0 else 0
+
+    def calculate_quality_rating_avg(self):
+        completed_pos = self.purchaseorder_set.filter(status='completed').exclude(quality_rating__isnull=True)
+        return completed_pos.aggregate(Avg('quality_rating'))['quality_rating__avg'] or 0
+
+    def calculate_average_response_time(self):
+        completed_pos = self.purchaseorder_set.filter(status='completed', acknowledgment_date__isnull=False)
+        response_times = [po.calculate_response_time() for po in completed_pos]
+        return sum(response_times) / len(response_times) if response_times else 0
+
+    def calculate_fulfillment_rate(self):
+        all_pos = self.purchaseorder_set.count()
+        successfully_fulfilled_pos = self.purchaseorder_set.filter(status='completed', quality_rating__isnull=True).count()
+        return (successfully_fulfilled_pos / all_pos) * 100 if all_pos > 0 else 0
+    
+    
 
 class PurchaseOrder(models.Model):
     po_number = models.CharField(max_length=50, unique=True)
@@ -30,6 +52,12 @@ class PurchaseOrder(models.Model):
 
     def __str__(self):
         return f"{self.po_number} - {self.vendor.name}"
+    
+    # To calculate Response time
+    def calculate_response_time(self):
+        if self.acknowledgment_date:
+            return (self.acknowledgment_date - self.issue_date).total_seconds() / 3600
+        return None
     
 class HistoricalPerformance(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
